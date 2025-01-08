da33b2703ac472d51f423924b28f4d04d783e468
Ed Grosvenor <ed.grosvenor@wickedviral.com>
2025-01-08 15:47:31 +0100

    Add verbs-flux to the splitter

--- split.sh
+++ split.sh
@@ -21,7 +21,9 @@ git pull origin $CURRENT_BRANCH
 remote bench https://github.com/artisan-build/bench.git
 remote docsidian https://github.com/artisan-build/docsidian.git
 remote gh https://github.com/artisan-build/gh.git
+remote verbs-flux https://github.com/artisan-build/verbs-flux.git
 
 split 'packages/bench' bench
 split 'packages/docsidian' docsidian
 split 'packages/gh' gh
+split 'packages/verbs-flux' verbs-flux
