22b316ac8391b324044efc7b816acda3cce30b5b
Ed Grosvenor <ed.grosvenor@wickedviral.com>
2025-01-10 13:09:42 +0100

    Spike out kibble package

---
+++ packages/kibble/src/Commands/CreatePackageCommand.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Commands;
+
+use Illuminate\Console\Command;
+use function Laravel\Prompts\text;
+
+class CreatePackageCommand extends Command
+{
+    protected $signature = 'kibble:create-package';
+    protected $description = 'Create a new package and add it to GitHub and Packagist';
+
+    public function handle(): int
+    {
+        $name = text('What are we naming this package?');
+        $description = text('What does this package do?');
+
+        // Use GH to create a new repo from the template
+
+        // Import that repo into the packages directory
+
+        // Run string replacements to rename files and set up the correct class names, etc.
+
+
+
+        return self::SUCCESS;
+    }
+}
---
+++ packages/kibble/src/Providers/KibbleServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Support\ServiceProvider;
+
+class KibbleServiceProvider extends ServiceProvider
+{
+    public function register(): void
+    {
+        $this->mergeConfigFrom(__DIR__.'/../../config/kibble.php', 'kibble');
+    }
+
+    public function boot(): void
+    {
+        $this->publishes([
+            __DIR__.'/../../config/kibble.php' => config_path('kibble.php'),
+        ], 'kibble');
+    }
+}
