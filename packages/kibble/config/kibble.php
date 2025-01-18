42e1b10f753bf2a40009f9919496ee45491ee9e2
Ed Grosvenor <ed.grosvenor@wickedviral.com>
2025-01-18 23:03:29 +0100

    Go back to my personal access token

--- packages/kibble/config/kibble.php
+++ packages/kibble/config/kibble.php
@@ -4,6 +4,6 @@ return [
     'organization' => 'artisan-build',
     'template' => 'artisan-build/skeleton',
     'homepage' => 'https://artisan.community',
-    'github_token' => env('GITHUB_TOKEN'),
+    'github_token' => env('GH_TOKEN'),
     'github_username' => 'edgrosvenor',
 ];
