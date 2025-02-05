c89dc7197dec4b17d36005b4e43b521e942f4ae0
Len Woodward <Len@ProjektGopher.com>
2025-02-05 01:36:02 -0800

    add state machine to DummyModel for testing and stan

--- packages/fat-enums/tests/Fixtures/DummyModel.php
+++ packages/fat-enums/tests/Fixtures/DummyModel.php
@@ -4,11 +4,15 @@ declare(strict_types=1);
 
 namespace ArtisanBuild\FatEnums\Tests\Fixtures;
 
+use ArtisanBuild\FatEnums\StateMachine\HasStateMachine;
 use Illuminate\Database\Eloquent\Model;
 
 class DummyModel extends Model
 {
+    use HasStateMachine;
     use \Sushi\Sushi;
+
+    protected $state_machines = [];
 
     protected $rows = [
         ['id' => 1111, 'name' => 'Foo'],
