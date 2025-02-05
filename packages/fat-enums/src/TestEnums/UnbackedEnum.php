b82ee674f336b4bf2be893bca08b4f9dad524829
Len Woodward <Len@ProjektGopher.com>
2025-02-04 19:02:19 -0800

    fill in tests for HasBackedEnumMethods trait

--- packages/fat-enums/src/TestEnums/UnbackedEnum.php
+++ packages/fat-enums/src/TestEnums/UnbackedEnum.php
@@ -4,8 +4,12 @@ declare(strict_types=1);
 
 namespace ArtisanBuild\FatEnums\TestEnums;
 
+use ArtisanBuild\FatEnums\Traits\HasBackedEnumMethods;
+
 enum UnbackedEnum
 {
+    use HasBackedEnumMethods;
+
     case Yes;
     case No;
     case Maybe;
---
+++ packages/fat-enums/tests/Unit/Traits/HasBackedEnumMethodsTest.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+use ArtisanBuild\FatEnums\TestEnums\UnbackedEnum;
+
+it('can get a case by name', function (): void {
+    expect(UnbackedEnum::tryFrom('Yes'))->toBe(UnbackedEnum::Yes);
+    expect(UnbackedEnum::from('No'))->toBe(UnbackedEnum::No);
+});
+
+it('returns null when getting a case by name that does not exist', function (): void {
+    expect(UnbackedEnum::tryFrom('Missing'))->toBeNull();
+});
+
+it('throws an exception when getting a case by name that does not exist', function (): void {
+    expect(fn () => UnbackedEnum::from('Missing'))->toThrow(\Exception::class);
+});
