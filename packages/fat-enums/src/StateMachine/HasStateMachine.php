bbb498a28ebd42957212732edbf901da95f2e0bd
Len Woodward <Len@ProjektGopher.com>
2025-02-05 15:20:08 -0800

    extract model specific behaviour for HasStateMachine trait

--- packages/fat-enums/src/StateMachine/HasStateMachine.php
+++ packages/fat-enums/src/StateMachine/HasStateMachine.php
@@ -4,7 +4,6 @@ namespace ArtisanBuild\FatEnums\StateMachine;
 
 use BackedEnum;
 use Exception;
-use Illuminate\Database\Eloquent\Model;
 use InvalidArgumentException;
 use ReflectionClass;
 use ReflectionClassConstant;
@@ -12,63 +11,6 @@ use ReflectionNamedType;
 
 trait HasStateMachine
 {
-    public static function bootHasStateMachine(): void
-    {
-        // @phpstan-ignore-next-line
-        if (! is_subclass_of(static::class, Model::class, true)) {
-            throw new Exception('The HasStateMachine::bootHasStateMachine() method can only be used on Eloquent models.');
-        }
-
-        if (
-            ! property_exists(static::class, 'state_machines')
-            || self::getNonUnionNonIntersectionType('state_machines', allow_null: true) !== 'array'
-        ) {
-            throw new Exception('The HasStateMachine::bootHasStateMachine() method can only be used on models that define a $state_machines array property.');
-        }
-
-        static::updating(function (Model $model): void {
-            $casts = $model->getCasts();
-
-            foreach ($model->getDirty() as $attribute => $value) {
-                if (in_array($attribute, $model->state_machines)) {
-                    // Make sure the attribute exists in $casts array
-                    if (! array_key_exists($attribute, $casts)) {
-                        throw new InvalidArgumentException("{$attribute} is not a valid cast on ".$model::class);
-                    }
-
-                    // Make sure it's an enum
-                    if (! enum_exists($casts[$attribute])) {
-                        throw new InvalidArgumentException("{$casts[$attribute]} is not a valid enum.");
-                    }
-
-                    // Make sure it implements the StateMachine contract
-                    if (! in_array(StateMachine::class, class_implements($casts[$attribute]))) {
-                        throw new InvalidArgumentException("{$casts[$attribute]} does not implement ".StateMachine::class);
-                    }
-
-                    if (is_string($value) || is_int($value)) {
-                        $value = is_a($casts[$attribute], BackedEnum::class, true)
-                            ? $casts[$attribute]::from($value)
-                            : $casts[$attribute]::{$value};
-                    }
-
-                    $original = $model->getOriginal($attribute);
-
-                    if (! $original->canTransitionTo($value)) {
-                        throw new InvalidStateTransition(sprintf(
-                            InvalidStateTransition::MESSAGE,
-                            $original->value,
-                            $value->value,
-                            $model::class,
-                            $attribute,
-                            $casts[$attribute],
-                        ));
-                    }
-                }
-            }
-        });
-    }
-
     public static function onlyRunInVerbsState(): void
     {
         if (! is_a(static::class, \Thunk\Verbs\State::class, true)) {
